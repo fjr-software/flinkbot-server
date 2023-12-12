7c99b9e61a982ea1cd6ba59a273982584849abaf
Francisco Júnior <franciscojr.dev@gmail.com>
2023-12-12 00:19:59 -0300

    Change

--- src/Handler.php
+++ src/Handler.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace FjrSoftware\FlinkbotServer\Server;
+namespace FjrSoftware\Flinkbot\Server;
 
 use Exception;
 use InvalidArgumentException;
--- src/Server.php
+++ src/Server.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace FjrSoftware\FlinkbotServer\Server;
+namespace FjrSoftware\Flinkbot\Server;
 
 use LogicException;
 use React\EventLoop\Loop;
